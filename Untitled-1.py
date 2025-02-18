cca316844f836b1593398f337dcd716f82a23012
AmaliaLinux <sotoes.amalia@gmail.com>
2025-02-18 07:23:47 -0600

    clase 17 Feb-25

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,19 @@
+nombre="Alex" 
+edad= 32
+altura=1.8
+print("Tu nombre actual es: "+ nombre)
 
+print("Dame tu uevo nombre : ")
+#nombre=input("¿Cual es tu nombe? ")
+
+print("Tu nombre actual es: "+ nombre)
+#Tengo barcos que permiten subir a 50 personas y hay una inundación y quiero que me digas el numero de 
+#personas que necesitan ser evacuadas y me diga cuantos barcos necesittas no lobran salvarse
+capacidad_barco=556
+capacidad_barco = int(input("Ingrese el número de personas a evacuar: "))
+capacidad_salvavidas=50
+botes_salvavidas=capacidad_barco//capacidad_salvavidas
+moridos=capacidad_barco%capacidad_salvavidas
+print(botes_salvavidas)
+print("Y se murieron: "+ str(moridos))
+ #de que tamaño es su barco y hacer el calculo
---
+++ if.py
@@ -0,0 +1,48 @@
+#Eres una billetera del metro,el bolero vale 5 pesos,Pero si eres menor de edad cuesta 4
+#Y si tienesaineres mayor d3 65 años tienes que cobrar 2 pesos, Solo le preguntas a la gente su edad
+
+age=0
+tickets_mars=0
+age=int(input("Ingresa la edad de la persona que va a comprar un ticket: "))
+if 0<age<19:
+    tickets_mars =4
+elif age>65:
+    tickets_mars=2
+else:
+    tickets_mars=5
+print("El costo del boleto es:"+str(tickets_mars))
+
+
+#La persona da ua edad y dependiendo la edad de de da un boleto diferente 
+
+#Escribe un programa que pida dos números y muestre cuál es el mayor. Si son iguales, indícalo.
+number1=int(input("Ingresa un numero: "))
+number2=int(input("Ingresa otro numero: "))
+if number1==number2:
+    print(f"Ambos numeros son iguales: {number1 }es igual que {number2}")
+elif number1>number2:
+    print("EL primer numero es mayor que el segundo")
+else:
+    print(f"El segundo numero es mayor al primero")
+
+
+# Crear un programa que calcule un impuesto, pides la cantidad, y se cobra de 1 a 100 mil el 5%
+# de 100 mil a 600 mil el 12% y de 600 mil en adelante el 20%, de manera escalonada
+
+salario=int(input("¿Cuánto ganas?"))
+pago=0
+if salario>600000:
+    pago+=(salario-600000)*.20
+    salario=600000
+    print(f"Tu pago de impuestos es {pago}")
+
+if salario>100000:
+    pago+=(salario-100000)*.125
+    salario=100000
+    print(f"Tu pago de impuestos es {pago}")
+
+if 0 <salario<=100000:
+    pago+=(salario)*.05
+print(f"Tu pago de impuestos es {pago}")
+
+print(f"Tu pago de impuestos total es {pago}")
