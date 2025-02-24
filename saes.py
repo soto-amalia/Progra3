0dc15ab142f3eb3177b688d00911c6a8489da03f
AmaliaLinux <sotoes.amalia@gmail.com>
2025-02-23 22:00:26 -0600

    funciones axuliares y variables locales

--- saes.py
+++ saes.py
@@ -8,17 +8,30 @@ alumno={
     "ocupacion":"Rey del pop"
 }
 
+
+def obtener_edad():
+    while True:#hasta que te de una entrada valida
+        edad=input("Ingresa tu edad:")
+        if edad.isdigit():
+            return int(edad)#inte es una funcion que convierte strings a enteros
+        else:
+            print("La edad debe de ser un numero")
+
+
 def registrar_alumno():#def declara una funcion
-    alumno={
+    alumno_variableLocal={
     "nombre":input("Ingresa tu nombre:"),
-    "apellido":input("Ingresa tu apelligo:"),
-    "edad":input("Ingresa tu edad:"),
+    "apellido":input("Ingresa tu apellido:"),
+    "edad":obtener_edad(),
     "genero":input("Ingresa tu genero:"),
     "direccion":input("Ingresa tu direccion"),
     "correo":input("Ingresa tu correo:"),
     "ocupacion":input("Ingresa tu ocupacion:"),
-}
-registrar_alumno()
+    }
+    return alumno_variableLocal #Aqui retornamos el diccionario llenado
+
+
+alumno = registrar_alumno()
 
 print("El perfil del alumno: ")
 for clave, valor in alumno.items(): #.items se usa pra recorrer el diccionario
